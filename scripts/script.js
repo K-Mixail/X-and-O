d4d49241744819ca697da9f2e7798bc0c872151f
K-Mixail <131530@mail.ru>
2023-02-03 20:20:56 +0300

    промежуточный вариант

--- scripts/script.js
+++ scripts/script.js
@@ -64,7 +64,7 @@ function testOn () {
   chooseLvl.removeAttribute('hidden');
 }
 
-startGame.addEventListener('click',testF,{once: true}); //{once: true} - функция выпол. только один раз
+startGame.addEventListener('click',ThreeO,{once: true}); //{once: true} - функция выпол. только один раз
 
 startGame.addEventListener('click',toggleStart,false);
 stopGame.addEventListener('click',toggleStop,false);
@@ -93,9 +93,8 @@ function clear () {
 }
 
 
-
 //ОДИНОЧКА 3х3 Первый ход КРЕСТИКОМ
-function testF () { 
+function ThreeX () { 
   let player = "x"; 
   let stat = {
     'x': 0,
@@ -104,10 +103,10 @@ function testF () {
   }
   // добавляем каждой ячейки событие клика (по клику сработает функция cellClick)
   for (let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', cellClick, true);
+    cell[i].addEventListener('click', cellClickX, true);
   }
 
-  function cellClick() {  
+  function cellClickX() {  
     let data = [];
     //если ячейка свободна, записываем в неё текущего игрока, если занята - выдаём сообщение
     if(!this.innerHTML) { 
@@ -171,107 +170,162 @@ function testF () {
 }
 
 // *************************ОДИНОЧКА 3х3 Первый ход НОЛИКОМ******************************
-// function testO () { 
+function ThreeO () { 
+  let player = "o"; 
+  let stat = {
+    'x': 0,
+    'o': 0,
+    'd': 0  
+  }
+  // добавляем каждой ячейки событие клика (по клику сработает функция cellClick)
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('click', cellClickO, true);
+  }
 
-//   let player = "o"; //начальный игрок (let т.к. Х будет меняться на 0)
-  
-//   //начальные значения статистики
-//   let stat = {
-//     'x': 0,
-//     'o': 0,
-//     'd': 0  //ничья
-//   }
-  
-//   //массив с выигрышными положениями
-//   const winIndex = [
-//     [1,2,3],
-//     [4,5,6],
-//     [7,8,9],
-//     [1,4,7],
-//     [2,5,8],
-//     [3,6,9],
-//     [1,5,9],
-//     [3,5,7],
-//   ];
-  
-//   // добавляем каждой ячейки событие клика (по клику сработает функция cellClick)
-//   for (let i = 0; i < cell.length; i++) {
-//     cell[i].addEventListener('click', cellClick, false);
-//   }
-  
-//   function cellClick() {  
-//     let data = [];
-//     //если ячейка свободна, записываем в неё текущего игрока, если занята - выдаём сообщение
-//     if(!this.innerHTML) { 
-//       this.innerHTML = player;
-//     } else {
-//       alert("Ячейка занята");
-//       return;
-//     }
-//     //проходим по ячейкам и если в ячейке стоит позиция текущего игрока, добавляем эти данные в массив data
-//     for(let i in cell) {
-//       if(cell[i].innerHTML == player) {
-//           data.push(parseInt(cell[i].getAttribute('pos')));
-//       }
-//     }
-  
-//     //проверяем, совпадаем ли текущее положение игрока с выигрышным или ничья
-//     // с помощью функции checkWin(data)
-//     if(checkWin(data)) {
-//       stat[player] += 1; //добавляем 1 к статистике победившего игрока
-//       restart("Выиграл: " + player); 
-//     }else {
-//       var draw = true;
-//       for(var i in cell) {
-//           if(cell[i].innerHTML == '') draw = false;
-//       }
-//       if(draw) {
-//           stat.d += 1;
-//           restart("Ничья");
-//       }
-//     } 
-  
-//     // после каждого хода меняем игрока 
-//     player = player == "o" ? "x" : "o";
-  
-//     //выводим на экран игрока, которй сейчас будет делать ход (O или Х)
-//     currentPlayer.innerHTML = player.toUpperCase();
-  
-//   }
-  
-//   //функция для проверки победы
-//   function checkWin(data) {
-//     for(let i in winIndex) {
-//       let win = true;
-//         for(let j in winIndex[i]) {
-//           let id = winIndex[i][j];
-//           let ind = data.indexOf(id);
-//           if(ind == -1) {
-//               win = false;
-//           }
-//         }
-//         if(win) return true;
-//     }
-//     return false;
-//   }
-  
-  
-//   //очищаем поле после каждой игры
-//   function restart(text) {    
-//     alert(text);
-//     for(let i = 0; i < cell.length; i++) {
-//         cell[i].innerHTML = '';
-//     }
-//     updateStat();
-//   }
-  
-//   function updateStat() {
-//     document.getElementById('statX').innerHTML = stat.x;
-//     document.getElementById('statO').innerHTML = stat.o;
-//     document.getElementById('statD').innerHTML = stat.d;
-//   }
-  
-//   }
+  function cellClickO() {  
+    let data = [];
+    //если ячейка свободна, записываем в неё текущего игрока, если занята - выдаём сообщение
+    if(!this.innerHTML) { 
+      this.innerHTML = player;
+    } else {
+      alert("Ячейка занята");
+      return;
+    }
+    //проходим по ячейкам и если в ячейке стоит позиция текущего игрока, добавляем эти данные в массив data
+    for(let i in cell) {
+      if(cell[i].innerHTML == player) {
+          data.push(parseInt(cell[i].getAttribute('pos')));
+      }
+    }
+    //проверка текущего положения на выигрыш/ничью с помощью функции checkWin(data)
+    if(checkWin(data)) {
+      stat[player] += 1; //добавляем 1 к статистике победившего игрока
+      restart("Выиграл: " + player);
+      player = "x" ;  // ЕСЛИ УБРАТЬ ЭТУ СТРОКУ - БУДЕТ ЧЕРЕДОВАНИЕ НАЧАЛЬНЫХ ХОДОВ !!!!!!!!!!!!!!!!!!!!
+    } else {
+      var draw = true;
+      for(var i in cell) {
+          if(cell[i].innerHTML == '') draw = false;
+      }
+      if(draw) {
+          stat.d += 1;
+          restart("Ничья");
+      }
+    } 
+    player = player == "o" ? "x" : "o"; // после каждого хода меняем игрока
+    currentPlayer.innerHTML = player.toUpperCase();//выводим игрока, который сейчас ходит
+  }
+
+  function checkWin(data) {
+    for(let i in winIndex) {
+      let win = true;
+        for(let j in winIndex[i]) {
+          let id = winIndex[i][j];
+          let ind = data.indexOf(id);
+          if(ind == -1) {
+              win = false;
+          }
+        }
+        if(win) return true;
+    }
+    return false;
+  }
+
+  function restart(text) {    
+    alert(text);
+    for(let i = 0; i < cell.length; i++) {
+        cell[i].innerHTML = '';
+    }
+    updateStat();
+  }
+  function updateStat() {
+    document.querySelector('#statX').innerHTML = stat.x;
+    document.querySelector('#statO').innerHTML = stat.o;
+    document.querySelector('#statD').innerHTML = stat.d;
+  }
+}
+
+// *************************ОДИНОЧКА 3х3 Чередование Первых Ходов******************************
+function Three () { 
+  let player = "x"; 
+  let stat = {
+    'x': 0,
+    'o': 0,
+    'd': 0  
+  }
+  // добавляем каждой ячейки событие клика (по клику сработает функция cellClick)
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('click', cellClick, true);
+  }
+
+  function cellClick() {  
+    let data = [];
+    //если ячейка свободна, записываем в неё текущего игрока, если занята - выдаём сообщение
+    if(!this.innerHTML) { 
+      this.innerHTML = player;
+    } else {
+      alert("Ячейка занята");
+      return;
+    }
+    //проходим по ячейкам и если в ячейке стоит позиция текущего игрока, добавляем эти данные в массив data
+    for(let i in cell) {
+      if(cell[i].innerHTML == player) {
+          data.push(parseInt(cell[i].getAttribute('pos')));
+      }
+    }
+    //проверка текущего положения на выигрыш/ничью с помощью функции checkWin(data)
+    if(checkWin(data)) {
+      stat[player] += 1; //добавляем 1 к статистике победившего игрока
+      restart("Выиграл: " + player);
+    } else {
+      var draw = true;
+      for(var i in cell) {
+          if(cell[i].innerHTML == '') draw = false;
+      }
+      if(draw) {
+          stat.d += 1;
+          restart("Ничья");
+      }
+    } 
+    player = player == "o" ? "x" : "o"; // после каждого хода меняем игрока
+    currentPlayer.innerHTML = player.toUpperCase();//выводим игрока, который сейчас ходит
+  }
+
+  function checkWin(data) {
+    for(let i in winIndex) {
+      let win = true;
+        for(let j in winIndex[i]) {
+          let id = winIndex[i][j];
+          let ind = data.indexOf(id);
+          if(ind == -1) {
+              win = false;
+          }
+        }
+        if(win) return true;
+    }
+    return false;
+  }
+
+  function restart(text) {    
+    alert(text);
+    for(let i = 0; i < cell.length; i++) {
+        cell[i].innerHTML = '';
+    }
+    updateStat();
+  }
+  function updateStat() {
+    document.querySelector('#statX').innerHTML = stat.x;
+    document.querySelector('#statO').innerHTML = stat.o;
+    document.querySelector('#statD').innerHTML = stat.d;
+  }
+}
+
+
+
+
+
+
+
 
 
 // *************************БОТ-ПРОСТОЙ (РАНДОМ)******************************
